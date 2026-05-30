789bf0d1d505fde5a27afcc765a247d103e2fbea
SVSPhoenix <tristanbonoan@icloud.com>
2026-05-30 11:58:49 -0700

    5/30/26
    working on player HP

--- David Sanchez Alva/1988/Assets/AurynSky/Dungeon Pack/Models/scripts/Player_HP.cs
+++ David Sanchez Alva/1988/Assets/AurynSky/Dungeon Pack/Models/scripts/Player_HP.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
-
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEditor;
 public class Player_HP : MonoBehaviour
 
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private int playing_hp;
     [SerializeField] private int max_hp;
+    [SerializeField] private List<Sprite> barhp;
+    [SerializeField] private Image heals;
+
         void Start()
     {
         playing_hp = max_hp;
@@ -19,10 +24,21 @@ public class Player_HP : MonoBehaviour
     public void Health(int damage)
     {
         playing_hp -= damage;
+        Debug.Log($"player hp = {playing_hp}");
+        changeHPbar();
       if (playing_hp <= 0)
       Debug.Log ("Player died");
         {
             //Destroy(gameObject);
         } 
+    }
+    
+    private void changeHPbar()
+    {
+        float hp_percent = (float)playing_hp / max_hp;
+        int index = barhp.Count - Mathf.RoundToInt(playing_hp * barhp.Count);
+        
+        Debug.Log (index);
+        heals.sprite = barhp[index];
     }
 }
